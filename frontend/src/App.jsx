fbd00676db1a92014aa7e3c48916ed9caf50a738
Andrey Anuchkin <uniqcle@yandex.ru>
2026-04-15 10:33:51 +0700

    components template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import BookForm from "./components/BookForm/BookForm";
+import BookList from "./components/BookList/BookList";
+import Filter from "./components/Filter/Filter";
 
 function App() {
-  
-
-  return (
-      <div className="app">
-          <header className="app-header">
-              <h1>Book Library App</h1>
-          </header>
-      </div>
-  );
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>Book Library App</h1>
+            </header>
+            <main className="app-main">
+                <div className="app-left-column">
+                    <BookForm />
+                </div>
+                <div className="app-right-column">
+                    {/* <Filter /> */}
+                    <BookList />
+                </div>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
